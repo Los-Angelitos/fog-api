eeb74a9cd8d14d54c35cea374578b46f51614206
Omar Morales <andrewmm45333@gmail.com>
2025-06-11 23:13:04 -0500

    feat(inventory): define inventory domain entities

--- inventory/domain/entities.py
+++ inventory/domain/entities.py
@@ -1,1 +1,17 @@
-﻿
+﻿class Supply:
+    def __init__(self, provider_id, name, price, stock, state, id=None):
+        self.id = id
+        self.provider_id = provider_id
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.state = state
+
+
+class SupplyRequest:
+    def __init__(self, payment_owner_id, supply_id, count, amount, id=None):
+        self.id = id
+        self.payment_owner_id = payment_owner_id
+        self.supply_id = supply_id
+        self.count = count
+        self.amount = amount
