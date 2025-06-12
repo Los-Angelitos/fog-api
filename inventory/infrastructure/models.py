e5be9f07b25d8424772381efa229eaf1acf45b09
Omar Morales <andrewmm45333@gmail.com>
2025-06-11 23:14:53 -0500

    feat(inventory): add database models for inventory persistence

--- inventory/infrastructure/models.py
+++ inventory/infrastructure/models.py
@@ -1,1 +1,27 @@
-﻿
+﻿from peewee import Model, AutoField, CharField, DecimalField, IntegerField
+from shared.infrastructure.database import db
+
+
+class Supply(Model):
+    id = AutoField()
+    provider_id = CharField()
+    name = CharField()
+    price = DecimalField(max_digits=10, decimal_places=2)
+    stock = IntegerField()
+    state = CharField()
+
+    class Meta:
+        database = db
+        table_name = 'supplies'
+
+
+class SupplyRequest(Model):
+    id = AutoField()
+    payment_owner_id = CharField()
+    supply_id = IntegerField()
+    count = IntegerField()
+    amount = DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        database = db
+        table_name = 'supply_requests'
