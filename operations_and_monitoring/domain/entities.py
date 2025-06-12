6e1aaeb2316103456041560e7a4101ece7005fcd
Arian Rodriguez <arianmrv12@gmail.com>
2025-06-12 00:12:06 -0500

    add(operations): added model and entities for reservations

--- operations_and_monitoring/domain/entities.py
+++ operations_and_monitoring/domain/entities.py
@@ -12,3 +12,24 @@ class SmokeSensor(Device):
         super().__init__(device_id, ip_address, mac_address)
         self.last_analogic_value = last_analogic_value
         self.last_alert_time = last_alert_time
+
+class Booking:
+    def __init__(self, id: int, payment_customer_id: int, room_id: int, description: str, start_date: str, final_date: str, price_room: float, night_count:int, amount: float, state: str, preference_id: int = None):
+        self.id = id
+        self.payment_customer_id = payment_customer_id
+        self.room_id = room_id
+        self.description = description
+        self.start_date = start_date
+        self.final_date = final_date
+        self.price_room = price_room
+        self.night_count = night_count
+        self.amount = amount
+        self.state = state
+        self.preference_id = preference_id
+
+class Room:
+    def __init__(self, id: int, type_room_id: int, hotel_id: int, state: str):
+        self.id = id
+        self.type_room_id = type_room_id
+        self.hotel_id = hotel_id
+        self.state = state
--- operations_and_monitoring/infrastructure/models.py
+++ operations_and_monitoring/infrastructure/models.py
@@ -17,3 +17,30 @@ class SmokeSensor(Device):
         Column('last_analogic_value', Float, nullable=False),
         Column('last_alert_time', DateTime, nullable=True),
     )
+
+class Booking:
+    __table__ = Table(
+        'bookings',
+        db.meta,
+        Column('id', String, primary_key=True),
+        Column('payment_customer_id', String, nullable=False),
+        Column('room_id', String, nullable=False),
+        Column('description', String, nullable=True),
+        Column('start_date', DateTime, nullable=False),
+        Column('final_date', DateTime, nullable=False),
+        Column('price_room', Float, nullable=False),
+        Column('night_count', Float, nullable=False),
+        Column('amount', Float, nullable=False),
+        Column('state', String, nullable=False),
+        Column('preference_id', String, nullable=True)
+    )
+
+class Room:
+    __table__ = Table(
+        'rooms',
+        db.meta,
+        Column('id', String, primary_key=True),
+        Column('type_room_id', String, nullable=False),
+        Column('hotel_id', String, nullable=False),
+        Column('state', String, nullable=False)
+    )
