23c08bd9e2ec7e6048d54c34b8e9ae94dc1583bd
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-14 02:31:20 -0500

    feat(device): added entity to dict.

--- iam/domain/entities.py
+++ iam/domain/entities.py
@@ -3,5 +3,11 @@
         self.device_id = device_id
         self.api_key = api_key
 
+    def to_dict(self):
+        return {
+            "device_id": self.device_id,
+            "api_key": self.api_key
+        }
+
     def __repr__(self):
         return f"Device(device_id={self.device_id})"
