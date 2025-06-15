8c0133fd856b581df9fb7c620db69d8adcf0c143
Fabia Herrera <herrerafabia.190604@gmail.com>
2025-06-14 22:39:54 -0500

    feat: added the hotel model

--- organizational_management/infrastructure/models.py
+++ organizational_management/infrastructure/models.py
@@ -1,1 +1,14 @@
-﻿
+﻿from shared.infrastructure.database import db
+from sqlalchemy import Column, String, Integer
+
+class Hotel(db.Base):
+    __tablename__ = 'hotels'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    owner_id = Column(Integer, nullable=False)
+    name = Column(String(100), nullable=True)
+    description = Column(String(255), nullable=True)
+    email = Column(String(100), nullable=True)
+    address = Column(String(255), nullable=True)
+    phone = Column(String(50), nullable=True)
+    
+    
