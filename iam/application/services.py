b4dcac580213454bfa16aba6ff1edecc126290b3
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-14 21:58:32 -0500

    feat(iam): added get all devices by room id application method.

--- iam/application/services.py
+++ iam/application/services.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from iam.application.external.services import ExternalOperationAndMonitoringService
 from iam.domain.entities import Device
 from iam.domain.services import AuthService
 from iam.infrastructure.repositories import DeviceRepository
@@ -9,6 +10,7 @@ class AuthApplicationService:
     def __init__(self):
         self.device_repository = DeviceRepository()
         self.auth_service = AuthService()
+        self.external_operation_and_monitoring_service = ExternalOperationAndMonitoringService()
 
     def authenticate(self, device_id: str, api_key: str) -> bool:
         device: Optional[Device] = self.device_repository.find_by_id_and_api_key(device_id, api_key)
@@ -22,3 +24,6 @@ class AuthApplicationService:
 
     def get_device_by_id_and_api_key(self, device_id: str, api_key: str) -> Optional[Device]:
         return self.device_repository.find_by_id_and_api_key(device_id, api_key)
+    
+    def get_all_devices_by_room_id(self, room_id:int):
+        return self.external_operation_and_monitoring_service.fetch_all_devices_by_room_id(room_id)
