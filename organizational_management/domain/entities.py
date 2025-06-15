db031479244fc19938edad19b1df5cf4dcd009f4
Fabia Herrera <herrerafabia.190604@gmail.com>
2025-06-14 22:39:42 -0500

    feat: added the hotel entity

--- organizational_management/domain/entities.py
+++ organizational_management/domain/entities.py
@@ -1,1 +1,10 @@
-﻿
+﻿class Hotel:
+    def __init__(self, id: int, owner_id: int, name: str = None, description: str = None, email: str = None, address: str = None, phone: str = None):
+        self.id = id
+        self.owner_id = owner_id
+        self.name = name
+        self.description = description
+        self.email = email
+        self.address = address
+        self.phone = phone
+        
